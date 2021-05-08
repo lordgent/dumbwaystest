cc3506445527ee56ab8e626ff00ff66a80dc0e1d
lordgent <lorjenx08@gmail.com>
2021-05-08 17:18:41 +0700

    mengerjakan soal 3 patter persegi dan mengupdate soal 4 php

--- soal3/soal3.js
+++ soal3/soal3.js
@@ -1,17 +1,58 @@
 //pattern persegi
 
+//hasil parameter 5
+
 function persegi(panjang) {
 
-
-    let hasil = ''
+    let hasil = '';
     for(let i = 0; i < panjang; i++) {
 
         for (let a = 0; a < panjang; a++) {
-            hasil += '* ';
+            if(a % 2 ) {
+                hasil += ' = ';
+
+            } else {
+                hasil += ' * ';
+            }
         }
         hasil += '\n';
+        
     }
     return hasil;
 
 }
-console.log(persegi(11));
+console.log("hasil parameter draw image 5");
+console.log(persegi(5)); 
+
+
+console.log('\n');
+
+
+//hasil parameter 7
+
+function persegi2(panjangg) {
+
+    let hasil = '';
+    for(let i = 0; i < panjangg; i++) {
+
+        for (let b = 0; b < panjangg; b++) {
+            
+            if(b % 0 ) {
+                hasil += ' * ';
+            } else if(b % 1) {
+                hasil += ' = ';
+            } else if(b % 3) {
+                hasil += ' = '
+            } 
+            else {
+                hasil += ' * ';
+            }
+        }
+        hasil += '\n';
+        
+    }
+    return hasil;
+
+}
+console.log("hasil parameter draw image 7");
+console.log(persegi2(7)); 
